316a425457bdc5974b526a8fb0a97ed76f6efdde
wuwenrui <1024567671@qq.com>
2020-06-14 22:09:05 +0800

    最大容量的容器

--- Week01/MoreWater.java
+++ Week01/MoreWater.java
@@ -80,4 +80,19 @@ class MoreWater{
         return max;
     }
 
+    /**
+     *
+     * 双指针
+     *
+     */
+    private int method4(int[] height){
+        int max = 0;
+        for (int i = 0, j = height.length - 1; i < j; ) {
+            int minHeight = height[i] <= height[j] ? height[i++] : height[j--];
+            int area = (j - i + 1) * minHeight;
+            max = Math.max(max, area);
+        }
+        return max;
+    }
+
 }
